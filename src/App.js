76eff92791eea09802ef2e96e79a3735023ad667
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-10 00:36:48 +0530

    completed assignment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const food = ["Mobile Operating System", "Mobile Manufacturers"];
+  const os = ["Android", "Blackberry","iphone","Windows Phone"];
+  const Mfs = ["Samsung", "HTC","Micromax","Apple"];
   return (
-    <div className="App">
-      hellownp
+    <div className="App paddingL">
+        
+        <Food food="Mobile Operating System"></Food>
+        <ul>
+          {os.map((e)=>{
+            return <li>{e}</li>
+          })}
+        </ul>
+
+        <Food food="Mobile Manufacturers"></Food>
+        <ul>
+          {Mfs.map((e)=>{
+            return <li>{e}</li>
+          })}
+        </ul>
     </div>
   );
 }
 
+function Food(props){
+  return <p className='bold'>{props.food}</p>
+}
+
+// function Foodsub(props1){
+//   return <p>{props1.os}</p>
+// }
 export default App;
